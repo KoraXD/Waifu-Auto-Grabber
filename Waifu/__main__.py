28053e0cb209ad9cd11a4936d48aca0c45d18fdb
Kora <115209660+KoraXD@users.noreply.github.com>
2024-03-03 19:01:17 +0530

    .

--- Waifu/__main__.py
+++ Waifu/__main__.py
@@ -6,3 +6,13 @@
      ⚡ Dark | @IkariS0_0
      ⚡ Otazuki | @Otazuki
 """
+
+import pyrogram, asyncio
+from Waifu import waifu
+
+async def main():
+    await waifu.start()
+    await pyrogram.idle()
+
+if __name__ == "__main__":
+    asyncio.run(main())
